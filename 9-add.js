248642f84cb23c1bafa2d06ee58766f8866b0099
[ingelcodes] <[ingelcodes@gmail.com]>
2025-07-19 16:23:33 +0000

    answered question 9-add.js

---
+++ 9-add.js
@@ -0,0 +1,9 @@
+ const firstInt = process.argv[2];
+const secondint = process.argv[3];
+
+function add(a, b) {
+ return  Number(a) + Number(b) 
+ }
+ const results= add(firstInt, secondint);
+
+  console.log(results);
